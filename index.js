b0f3a315b25cde940cf879f3656e76a50957606d
zhangzhonghao <zhangzhonghao@kuaishou.com>
2023-04-21 18:58:26 +0800

    feat: update rewrite

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+
 app.use('/githubApi', createProxyMiddleware({ 
   target: 'https://github.com',
-  changeOrigin: true
+  changeOrigin: true,
+  pathRewrite: {
+    '^/githubApi': ''
+  }
 }));
+
+app.use('*', (req, res) => {
+    res.send({ result: -1 });
+})
 
 app.listen(8899, () => {
   console.log('Server started on http://localhost:8899');
